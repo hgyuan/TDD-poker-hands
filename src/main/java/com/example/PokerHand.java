5138314e5b2e50f61063741edb74e10b78a12ebf
1764775976@qq.com <1764775976@qq.com>
2020-08-26 14:55:07 +0800

    [TDD-poker-hands][richard, front]feature: pass should_return_black_wins_when_play_given_black_2H_3D_5S_9C_KD_and_white_2C_3H_4S_8C_AH

---
+++ src/main/java/com/example/PokerHand.java
@@ -0,0 +1,54 @@
+package com.example;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PokerHand {
+
+  private Map<Character, Integer> map;
+
+  public PokerHand(){
+    initMap();
+  }
+
+  public String play(String[] black, String[] white){
+    int[] blackNumbers = convertCardValueToNumber(black);
+    int[] whiteNumbers = convertCardValueToNumber(white);
+
+    Arrays.sort(blackNumbers);
+    Arrays.sort(whiteNumbers);
+
+    if (blackNumbers[4] > whiteNumbers[4]){
+      return "black wins";
+    }
+    return "white wins";
+  }
+
+  private int[] convertCardValueToNumber(String[] card) {
+    int[] numbers = new int[5];
+    for(int i=0; i<5; i++){
+      char cardValue = card[i].charAt(0);
+      numbers[i] = map.get(cardValue);
+    }
+    return numbers;
+  }
+
+  private void initMap(){
+    map = new HashMap<Character, Integer>();
+    map.put('1', 1);
+    map.put('2', 2);
+    map.put('3', 3);
+    map.put('4', 4);
+    map.put('5', 5);
+    map.put('6', 6);
+    map.put('7', 7);
+    map.put('8', 8);
+    map.put('9', 9);
+    map.put('T', 10);
+    map.put('J', 11);
+    map.put('Q', 12);
+    map.put('K', 13);
+    map.put('A', 14);
+  }
+}
---
+++ src/test/java/com/example/PokerHandTest.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class PokerHandTest {
+
+  /*
+  given black: [2H, 3D, 5S, 9C, KD] white:[2C, 3H, 4S, 8C, AH]
+  when porkerHand.play(black, white)
+  then black wins
+   */
+  @Test
+  public void should_return_black_wins_when_play_given_black_2H_3D_5S_9C_KD_and_white_2C_3H_4S_8C_AH() {
+    //given
+    String[] black = {"2H", "3D", "5S", "9C", "KD"};
+    String[] white = {"2C", "3H", "4S", "8C", "AH"};
+    //when
+    PokerHand pokerHand = new PokerHand();
+    String result = pokerHand.play(black, white);
+
+    //then
+    assertEquals(result, "white wins");
+  }
+}
