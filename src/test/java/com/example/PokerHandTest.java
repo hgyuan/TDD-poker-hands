e7288fa53b12bb3904f2613fee7e2d7f2e72ac17
1764775976@qq.com <1764775976@qq.com>
2020-08-26 21:47:14 +0800

    [TDD-poker-hands][richard, front]feature: pass should_return_white_wins_when_play_given_black_3H_3S_6C_6H_5H_and_white_4C_3D_4S_6D_4D

--- src/test/java/com/example/PokerHandTest.java
+++ src/test/java/com/example/PokerHandTest.java
@@ -363,4 +363,17 @@ public class PokerHandTest {
     assertEquals(result, BLACK_WINS);
   }
 
+  @Test
+  public void should_return_white_wins_when_play_given_black_3H_3S_6C_6H_5H_and_white_4C_3D_4S_6D_4D() {
+    //given
+    String[] black = {"3H", "3S", "6C", "6H", "5H"};
+    String[] white = {"4C", "3D", "4S", "6D", "4D"};
+    //when
+    PokerHand pokerHand = new PokerHand();
+    String result = pokerHand.play(black, white);
+
+    //then
+    assertEquals(result, WHITE_WINS);
+  }
+
 }
