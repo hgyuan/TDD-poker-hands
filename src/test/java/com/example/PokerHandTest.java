3a2dd510daa86e4e834950f401652fa330e3807f
1764775976@qq.com <1764775976@qq.com>
2020-08-26 21:41:08 +0800

    [TDD-poker-hands][richard, front]feature: pass should_return_tie_when_play_given_black_3H_3S_6C_6H_2H_and_white_3C_3D_6S_6D_2D

--- src/test/java/com/example/PokerHandTest.java
+++ src/test/java/com/example/PokerHandTest.java
@@ -336,4 +336,17 @@ public class PokerHandTest {
     //then
     assertEquals(result, BLACK_WINS);
   }
+
+  @Test
+  public void should_return_tie_when_play_given_black_3H_3S_6C_6H_2H_and_white_3C_3D_6S_6D_2D() {
+    //given
+    String[] black = {"3H", "3S", "6C", "6H", "2H"};
+    String[] white = {"3C", "3D", "6S", "6D", "2D"};
+    //when
+    PokerHand pokerHand = new PokerHand();
+    String result = pokerHand.play(black, white);
+
+    //then
+    assertEquals(result, TIE);
+  }
 }
